99754dce3f2d8ef27d6c605c4b8710c0eaa7c8f8
James Counter <james.counter@gmail.com>
2017-01-08 15:18:34 +0000

    Added grunt concat task to concat all JS into bundle then to run uglify on the bundle

--- public/Gruntfile.js
+++ public/Gruntfile.js
@@ -4,13 +4,21 @@ module.exports = function(grunt) {
     
     pkg: grunt.file.readJSON('package.json'),
     
+    concat: {
+      js: {
+        files: {
+          'dist/js/bundle.js': ['node_modules/jquery/dist/jquery.js', 'dev/javascript/bootstrap.js', 'dev/app/**/*.js']
+        }
+      }
+    },
+
     uglify: {
       options: {
         manage: 'false',
       },
       my_targets: {
         files: {
-          'dist/js/main.min.js' : ['node_modules/jquery/dist/jquery.js', 'dev/javascript/bootstrap.js', 'dev/app/**/*.js']
+          'dist/js/main.min.js' : ['dist/js/bundle.js']
         }
       }
     },    
@@ -36,6 +44,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.registerTask('default',['watch']);
-
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.registerTask('default',['concat', 'sass', 'uglify', 'watch']);
 }
